76dbd9dd336cd77cfcdbc3c53d181084f8bc4ea8
Oni <oni-mail@mail.ru>
2022-12-13 21:31:25 +0300

    Example 5 spec user 2

--- Example005_Masha/Program.cs
+++ Example005_Masha/Program.cs
@@ -1,2 +1,12 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+string user_name = Console.ReadLine();
+if (user_name == "Маша")
+{
+    Console.WriteLine("Hello !!!!!!!!!!!!!!");
+}
+else
+{
+    Console.Write("Hi, ");
+    Console.WriteLine(user_name);
+}
