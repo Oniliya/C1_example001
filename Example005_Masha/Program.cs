3839fb1f3690977964635ecc5911ee203e7a727a
Oni <oni-mail@mail.ru>
2022-12-13 21:36:45 +0300

    Example 5 spec user 3 lower

--- Example005_Masha/Program.cs
+++ Example005_Masha/Program.cs
@@ -1,12 +1,12 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+Console.WriteLine("Введите имя пользователя: ");
 string user_name = Console.ReadLine();
-if (user_name == "Маша")
+if (user_name.ToLower() == "маша")
 {
-    Console.WriteLine("Hello !!!!!!!!!!!!!!");
+    Console.WriteLine("Ура! Это же Маша!");
 }
 else
 {
-    Console.Write("Hi, ");
+    Console.Write("Привет, ");
     Console.WriteLine(user_name);
 }
