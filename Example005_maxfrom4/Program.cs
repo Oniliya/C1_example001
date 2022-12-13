b2c313b18cfcf3e58cd276347f0161b63db7b7fa
Oni <oni-mail@mail.ru>
2022-12-13 21:50:30 +0300

    Example 6 max 5

--- Example005_maxfrom4/Program.cs
+++ Example005_maxfrom4/Program.cs
@@ -2,5 +2,28 @@
 int b=8;
 int c=2;
 int d=5;
-max_f=a;
-if (max_f > )
+int e=7;
+int max_f=a;
+if (a > max_f  )
+{
+    max_f=a;
+};
+if (b > max_f  )
+{
+    max_f=b;
+};
+if (c > max_f  )
+{
+    max_f=c;
+};
+if (d > max_f  )
+{
+    max_f=d;
+};
+if (e > max_f  )
+{
+    max_f=e;
+};
+
+Console.Write("Максимальное из 4х: ");
+Console.WriteLine(max_f);
