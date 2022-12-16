1318c5566eeaf6f8cb9cf44c195276bebb28cb65
Oni <oni-mail@mail.ru>
2022-12-16 19:01:47 +0300

    hw 1

---
+++ Sem_1_hw1/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.Write("Введите первое число = ");
+int a1 = int.Parse(Console.ReadLine()); 
+Console.Write("Введите второе число = ");
+int a2 = int.Parse(Console.ReadLine()); 
+if (a1>a2)
+{
+    Console.WriteLine($"max = {a1}");
+} 
+else 
+{
+    if (a1==a2)
+    {
+        Console.WriteLine($"{a1}={a2}");
+    }
+    else
+    {
+        Console.WriteLine($"max = {a2}");
+    }
+}
