23f7f9ca6d4eb9eca34d7c1cbe3464e0d4de9c09
Oni <oni-mail@mail.ru>
2022-12-16 19:17:07 +0300

    hw 3

---
+++ Sem_1_hw3/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Введите число = ");
+int a1 = int.Parse(Console.ReadLine()); 
+
+if (a1%2==0)
+{
+    Console.WriteLine($"{a1} - четное");
+} 
+else
+{
+    Console.WriteLine($"{a1} - не четное");
+}
