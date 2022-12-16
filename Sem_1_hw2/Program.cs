5ff6bddac9cd38b5ab844f265ebcac720a4ef969
Oni <oni-mail@mail.ru>
2022-12-16 19:11:22 +0300

    hw 2

---
+++ Sem_1_hw2/Program.cs
@@ -0,0 +1,23 @@
+﻿Console.Write("Введите первое число = ");
+int a1 = int.Parse(Console.ReadLine()); 
+Console.Write("Введите второе число = ");
+int a2 = int.Parse(Console.ReadLine()); 
+Console.Write("Введите третье число = ");
+int a3 = int.Parse(Console.ReadLine()); 
+
+if ((a1>a2)&&(a1>a3))
+{
+    Console.WriteLine($"max = {a1}");
+} 
+if ((a2>a1)&&(a2>a3))
+{
+    Console.WriteLine($"max = {a2}");
+} 
+if ((a3>a1)&&(a3>a2))
+{
+    Console.WriteLine($"max = {a3}");
+} 
+if ((a1==a2)&&(a1==a3))
+{
+    Console.WriteLine("числа равны");
+} 
