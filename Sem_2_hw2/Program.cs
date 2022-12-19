d08e98a3f849af84542cc25016ba78478e5a0371
Oni <oni-mail@mail.ru>
2022-12-19 22:49:49 +0300

    sem 2 hw2

---
+++ Sem_2_hw2/Program.cs
@@ -0,0 +1,31 @@
+﻿int a;
+while (true)
+{
+    Console.Write("Введите трехзначное число: ");
+    if ((int.TryParse(Console.ReadLine(), out a))&&(a>99))
+    {
+        int n=0;
+        int a_save=a;
+        while (n<2)
+        {
+            a=a/10;
+            n++;
+        }
+        Console.WriteLine($"третья цифра справа налево {a_save} -> {a%10}");
+     
+        a=a_save;
+        int a3=0;
+        while (a>99)
+        {
+            a3=a%10;
+            a=a/10;
+        }
+
+        //Console.WriteLine($"{a3}");
+        Console.WriteLine($"третья цифра слева направо {a_save} -> {a3}");
+        break;
+    }
+    Console.WriteLine("третьей цифры нет");
+
+}
+
