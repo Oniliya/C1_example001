0f57c7358825fd70b5c19db73325a0acace8e8de
Oni <oni-mail@mail.ru>
2022-12-19 22:24:34 +0300

    sem 2 hw1

---
+++ Sem_2_hw1/Program.cs
@@ -0,0 +1,13 @@
+﻿int a;
+while (true)
+{
+    Console.Write("Введите трехзначное число: ");
+    if ((int.TryParse(Console.ReadLine(), out a))&&(a>99)&&(a<1000))
+    {
+        int a2=(a%100)/10;
+        Console.WriteLine($"{a} -> {a2}");
+        break;
+    }
+    Console.WriteLine("Ошибка ввода!");
+}
+
--- Sem_2_work_ex1/Program.cs
+++ Sem_2_work_ex1/Program.cs
@@ -9,6 +9,7 @@
 // }
 // Console.WriteLine($"A={varA}");
 
+
 // int num=new Random().Next(10,100);
 // int a1=num/10;
 // int a2=num%10;
@@ -23,9 +24,11 @@
 
 // int num=new Random().Next(100,1000);
 // int a1=num/100;
-// int a3=(num%100)%10;
+// int a3=num%10;
 // int num2=a1*10+a3;
 // Console.WriteLine($"{num} -> {num2}");
+
+
 
 
 
@@ -33,9 +36,12 @@
 // int a1 = int.Parse(Console.ReadLine()); 
 // Console.Write("Введите второе число = ");
 // int a2 = int.Parse(Console.ReadLine()); 
-// if (a2%a1 > 0 )
+
+// int a3 = a2 % a1;
+
+// if (a3 > 0 )
 // {
-//    Console.WriteLine($"не кратно, остаток {a2%a1}"); 
+//    Console.WriteLine($"не кратно, остаток {a3}"); 
 // }
 // else
 // {
@@ -44,29 +50,33 @@
 
 
 
-// Console.Write("Введите первое число = ");
-// int a1 = int.Parse(Console.ReadLine()); 
-// if ((a1%23 ==0)&&(a1%7 ==0))
-// {
-//     Console.WriteLine($"да"); 
-// }
-// else
-// {
-//    Console.WriteLine($"Нет");  
-// }
+
+Console.Write("Введите первое число = ");
+int a1 = int.Parse(Console.ReadLine()); 
+if ((a1%23 ==0)&&(a1%7 ==0))
+{
+    Console.WriteLine($"да"); 
+}
+else
+{
+   Console.WriteLine($"Нет");  
+}
 
 
 
 
-// Console.Write("Введите первое число = ");
-// int a1 = int.Parse(Console.ReadLine()); 
-// Console.Write("Введите второе число = ");
-// int a2 = int.Parse(Console.ReadLine()); 
-// if ((a2==a1*a1)||(a1==a2*a2))
-// {
-//    Console.WriteLine($"да"); 
-// }
-// else
-// {
-//     Console.WriteLine($"нет"); 
-// }
+
+
+
+Console.Write("Введите первое число = ");
+int a1 = int.Parse(Console.ReadLine()); 
+Console.Write("Введите второе число = ");
+int a2 = int.Parse(Console.ReadLine()); 
+if ((a2==a1*a1)||(a1==a2*a2))
+{
+   Console.WriteLine($"да"); 
+}
+else
+{
+    Console.WriteLine($"нет"); 
+}
