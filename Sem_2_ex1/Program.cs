1a65a9c7d6c6701ccbd0fd07615e3527a8eb524a
Oni <oni-mail@mail.ru>
2022-12-19 19:33:44 +0300

    sem 2 ex 2

--- Sem_2_ex1/Program.cs
+++ Sem_2_ex1/Program.cs
@@ -1,2 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int Max(int ar1, int ar2,  int ar3)
+{
+ int result =ar1;
+ if (ar2>result) result = ar2;
+ if (ar3>result) result = ar3;
+ return result;
+}
+
+int a1 =12;
+int b1 =12;
+int c1 =12;
+
+int a2 =12;
+int b2 =312;
+int c2 =12;
+
+int c3 =12;
+int b3 =12;
+int a3 =12;
+
+
+
+
+//int max1=Max(a1, b1, c1);
+//int max2=Max(a2, b2, c2);
+//int max3=Max(a3, b3, c3);
+int max12= Max(Max(a1, b1, c1), Max(a2, b2, c2), Max(a3, b3, c3));
+
+
+
+Console.WriteLine(max12);
