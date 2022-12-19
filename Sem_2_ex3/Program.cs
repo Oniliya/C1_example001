3ea233ef288839ccb03a6fee40987442331a9070
Oni <oni-mail@mail.ru>
2022-12-19 19:46:36 +0300

    sem 2 ex 44

--- Sem_2_ex3/Program.cs
+++ Sem_2_ex3/Program.cs
@@ -1,12 +1,13 @@
-﻿int[] array={1,342,3,354,5355,635,7,8};
+﻿int[] array={1,342,3,8 ,8 ,8 ,354,4,635,7,8};
 int n = array.Length;
-int find = 4;
+int find = 8;
 int index = 0;
 while (index < n)
 {
     if (array[index]==find)
     {
-        
+       Console.WriteLine(index) ;
+       break;
     }
     index=index+1; //index++;
 }
