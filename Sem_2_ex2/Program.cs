0ed1684eb9d336d9fa8b920e8cf858ea1adb1248
Oni <oni-mail@mail.ru>
2022-12-19 19:43:10 +0300

    sem 2 ex 3

--- Sem_2_ex2/Program.cs
+++ Sem_2_ex2/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int Max(int ar1, int ar2,  int ar3)
+{
+ int result =ar1;
+ if (ar2>result) result = ar2;
+ if (ar3>result) result = ar3;
+ return result;
+}
+
+int [] array ={1,342,3345,4,345,635,7,8354,93333};
+array[0]=15;
+Console.WriteLine(array[0]);
+
+int max = Max(
+    Max(array[0],array[1],array[2]),
+    Max(array[3],array[4],array[5]),
+    Max(array[6],array[7],array[8])
+);
+Console.WriteLine(max);
---
+++ Sem_2_ex3/Program.cs
@@ -0,0 +1,12 @@
+﻿int[] array={1,342,3,354,5355,635,7,8};
+int n = array.Length;
+int find = 4;
+int index = 0;
+while (index < n)
+{
+    if (array[index]==find)
+    {
+        
+    }
+    index=index+1; //index++;
+}
