0ee376c7791a0fd6657be379574cd36e2b4f4ff7
Oni <oni-mail@mail.ru>
2022-12-19 20:51:08 +0300

    sem 2 work_ex1-2

--- Sem_2_work_ex1/Program.cs
+++ Sem_2_work_ex1/Program.cs
@@ -1,2 +1,72 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// double varA;
+
+// while (true)
+// {
+//         Console.Write("Введите А: ");
+//         if (double.TryParse(Console.ReadLine(), out varA))
+//               break;
+//         Console.WriteLine("Ошибка ввода!");
+// }
+// Console.WriteLine($"A={varA}");
+
+// int num=new Random().Next(10,100);
+// int a1=num/10;
+// int a2=num%10;
+// int max=a1;
+// if (max<a2)
+// {
+//     max = a2;
+// }
+// Console.WriteLine($"Максимальная цифра в числе {num} -> {max}");
+
+
+
+// int num=new Random().Next(100,1000);
+// int a1=num/100;
+// int a3=(num%100)%10;
+// int num2=a1*10+a3;
+// Console.WriteLine($"{num} -> {num2}");
+
+
+
+// Console.Write("Введите первое число = ");
+// int a1 = int.Parse(Console.ReadLine()); 
+// Console.Write("Введите второе число = ");
+// int a2 = int.Parse(Console.ReadLine()); 
+// if (a2%a1 > 0 )
+// {
+//    Console.WriteLine($"не кратно, остаток {a2%a1}"); 
+// }
+// else
+// {
+//     Console.WriteLine($"кратно"); 
+// }
+
+
+
+// Console.Write("Введите первое число = ");
+// int a1 = int.Parse(Console.ReadLine()); 
+// if ((a1%23 ==0)&&(a1%7 ==0))
+// {
+//     Console.WriteLine($"да"); 
+// }
+// else
+// {
+//    Console.WriteLine($"Нет");  
+// }
+
+
+
+
+// Console.Write("Введите первое число = ");
+// int a1 = int.Parse(Console.ReadLine()); 
+// Console.Write("Введите второе число = ");
+// int a2 = int.Parse(Console.ReadLine()); 
+// if ((a2==a1*a1)||(a1==a2*a2))
+// {
+//    Console.WriteLine($"да"); 
+// }
+// else
+// {
+//     Console.WriteLine($"нет"); 
+// }
