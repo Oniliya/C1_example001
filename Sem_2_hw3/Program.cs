fa4fa88889e755d20a4a8e69223ce5a99d35f8c2
Oni <oni-mail@mail.ru>
2022-12-19 22:54:31 +0300

    sem 2 hw3

---
+++ Sem_2_hw3/Program.cs
@@ -0,0 +1,19 @@
+﻿int a;
+while (true)
+{
+    Console.Write("Введите день недели: ");
+    if ((int.TryParse(Console.ReadLine(), out a))&&(a>0)&&(a<8))
+    {
+        if ((a==6)||(a==7))
+        {
+            Console.WriteLine($"{a} -> да");
+        }
+        else
+        {
+            Console.WriteLine($"{a} -> нет");
+        }
+        break;
+    }
+    Console.WriteLine("Ошибка ввода!");
+}
+
