619a8e07d718265a0da0dcf196a8acd71d829d9a
Oni <oni-mail@mail.ru>
2022-12-22 18:36:12 +0300

    sem 3 ex3

---
+++ Sem_3_ex3/Program.cs
@@ -0,0 +1,22 @@
+﻿static void Dots_Coord(ref int x, ref int y)
+{
+    try
+    {
+        Console.Write("Введите координату X : ");
+        x = int.Parse(Console.ReadLine() ?? "");
+        Console.Write("Введите координату Y : ");
+        y = int.Parse(Console.ReadLine() ?? "");
+    }
+    catch (Exception exc)
+    {
+        Console.WriteLine($"Ошибка ввода данных! {exc.Message}");
+        return;
+    }
+}
+
+int x1=0, y1=0, x2=0, y2=0;
+Dots_Coord(ref x1, ref y1);
+Dots_Coord(ref x2, ref y2);
+
+double res=Math.Sqrt(Math.Pow((x1-x2),2)+Math.Pow((y1-y2),2));
+Console.WriteLine($"{res}, ");
