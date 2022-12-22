ec6db3c3d74039ac32d2fe3d2ad8830e2c503704
Oni <oni-mail@mail.ru>
2022-12-22 18:43:16 +0300

    sem 3 ex4

---
+++ Sem_3_ex4/Program.cs
@@ -0,0 +1,23 @@
+﻿static void Init_Digit(ref int x)
+{
+    try
+    {
+        Console.Write("Введите число: ");
+        x = int.Parse(Console.ReadLine() ?? "");
+    }
+    catch (Exception exc)
+    {
+        Console.WriteLine($"Ошибка ввода данных! {exc.Message}");
+        return;
+    }
+}
+
+int N=0;
+Init_Digit(ref N);
+int index=1;
+Console.Write($"{N}->");
+while (index<=N)
+{
+    Console.Write($"{index*index}, ");
+    index++;
+}
