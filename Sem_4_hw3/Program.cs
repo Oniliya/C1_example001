c0b0f016ce932adc1cc23572f8069abeebb9968c
Oni <oni-mail@mail.ru>
2023-01-12 22:01:19 +0300

    Sem_4_hw3

---
+++ Sem_4_hw3/Program.cs
@@ -0,0 +1,38 @@
+﻿int GetNumberFromUser(string message, string errorMessage)
+{
+    while (true)
+    {
+        Console.Write(message);
+        bool isCorrect = int.TryParse(Console.ReadLine(), out int userNumber);
+        if (isCorrect)
+            return userNumber;
+        Console.WriteLine(errorMessage);
+    }
+}
+
+void PrintArray(int[] array)
+{
+    int count = array.Length;
+    Console.Write("[");
+    for (int i=0; i<count; i++)
+    {
+       if (i != count-1) Console.Write($"{array[i]},");
+       else  Console.Write($"{array[i]}]");
+    }
+    Console.WriteLine();
+}
+
+void CreateArray(int[] arr, int len)
+{
+    for (int i = 0; i < len; i++)
+    {
+        int n = GetNumberFromUser($"Введите {i+1}е число ", "Ошибка ввода!");
+        arr[i] = n; 
+    }
+}
+
+int[] arr = new int[8];
+CreateArray(arr,8);
+PrintArray(arr);
+
+
