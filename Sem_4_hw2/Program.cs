37db131626efbd90e29f60376cf13c9d11f25a0e
Oni <oni-mail@mail.ru>
2023-01-12 21:52:00 +0300

    Sem_4_hw2

---
+++ Sem_4_hw2/Program.cs
@@ -0,0 +1,31 @@
+﻿int GetNumberFromUser(string message, string errorMessage)
+{
+    while (true)
+    {
+        Console.Write(message);
+        bool isCorrect = int.TryParse(Console.ReadLine(), out int userNumber);
+        if (isCorrect)
+            return userNumber;
+        Console.WriteLine(errorMessage);
+    }
+}
+
+
+int Calc(int x)
+{
+    int summa=0;
+    while (x > 0)
+    {
+        summa=summa+x%10;
+        x = x / 10;
+    }
+    return summa;
+}
+
+
+int n = GetNumberFromUser("Введите целое число A: ", "Ошибка ввода!");
+Console.Write($"{n}->");
+
+int c = Calc(n);
+Console.WriteLine(c);
+
