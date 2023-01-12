7fc879ccf4d033205189a9a79a3d9d7c2e6b0b69
Oni <oni-mail@mail.ru>
2023-01-12 21:46:41 +0300

    Sem_4_all

--- Sem_4_ex1/Program.cs
+++ Sem_4_ex1/Program.cs
@@ -1,11 +1,11 @@
 ﻿int GetNumberFromUser(string message, string errorMessage)
-{ 
-    while(true)
+{
+    while (true)
     {
         Console.Write(message);
         bool isCorrect = int.TryParse(Console.ReadLine(), out int userNumber);
-        if(isCorrect)
-        return userNumber;
+        if (isCorrect)
+            return userNumber;
         Console.WriteLine(errorMessage);
     }
 }
@@ -13,30 +13,34 @@
 
 int Calc(int x)
 {
-    int count=0;
-    while (x>0)
+    int count = 0;
+    int summa=0;
+    while (x > 0)
     {
-        x=x/10;
+        //summa=summa+x%10;
+
+        x = x / 10;
         count++;
     }
     return count;
+    //return summa;
 }
 
 int Calc2(int x)
 {
-    int mult=1;
-    for (int i=1;i<=x;i++)
+    int mult = 1;
+    for (int i = 1; i <= x; i++)
     {
-        mult=mult*i;
+        mult = mult * i;
     }
     return mult;
-}    
+}
 
-int n = GetNumberFromUser("Введите целое число A: ","Ошибка ввода!");
+int n = GetNumberFromUser("Введите целое число A: ", "Ошибка ввода!");
 Console.Write($"{n}->");
 
-int c=Calc(n);
+int c = Calc(n);
 Console.WriteLine(c);
 
-int c2=Calc2(n);
+int c2 = Calc2(n);
 Console.WriteLine(c2);
