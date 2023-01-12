6f62b9d4cc1d20c738512d96aee0f52be0bbb23e
Oni <oni-mail@mail.ru>
2023-01-12 20:07:34 +0300

    Sem_4_ex1

---
+++ Sem_4_ex1/Program.cs
@@ -0,0 +1,23 @@
+﻿static int Init_Digit()
+{
+    while (true)
+    {
+        Console.Write("Введите число: ");
+        if (int.TryParse(Console.ReadLine(), out int x))
+        {
+            return x;
+            break;
+        }
+        Console.WriteLine("Ошибка ввода!");
+    }
+}
+
+int n=Init_Digit();
+Console.Write($"{n}->");
+int count=0;
+while (n>0)
+{
+    n=n/10;
+    count++;
+}
+Console.WriteLine(count);
