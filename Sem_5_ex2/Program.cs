ae031ed19ce14bb9f71d01d9f6b6cc1d4ff1efc9
Oni <oni-mail@mail.ru>
2023-01-16 19:55:25 +0300

    Sem_5_ex2

---
+++ Sem_5_ex2/Program.cs
@@ -0,0 +1,65 @@
+﻿//
+//Напишите программу замены элементов массива: положительные элементы замените на соответствующие отрицательные, и наоборот.
+//
+
+int[] GetArray(int size, int minValue, int maxValue)
+{
+    int[] res = new int[size];
+     for (int i = 0; i < size; i++)
+    {
+        res[i] = new Random().Next(minValue, maxValue + 1);
+    }
+    return res;
+}
+
+Console.Clear();
+int[] array = GetArray(4, -10, 10);
+Console.WriteLine(String.Join(" ", array));
+
+int[] ChangeArray(int[] arr)
+{
+    for (int i=0; i<arr.Length; i++)
+    {
+        arr[i]=-arr[i];
+    }
+    return arr;
+}
+
+int[] array2 = ChangeArray(array);
+Console.WriteLine(String.Join(" ", array));
+
+//
+//Задайте массив. Напишите программу, которая определяет, присутствует ли заданное число в массиве.
+//
+int GetNumberFromUser(string message, string errorMessage)
+{
+    while (true)
+    {
+        Console.Write(message);
+        bool isCorrect = int.TryParse(Console.ReadLine(), out int userNumber);
+        if (isCorrect)
+            return userNumber;
+        Console.WriteLine(errorMessage);
+    }
+}
+
+bool Check(int x, int[] arr)
+{
+    int i=0;
+    bool f=false;
+    while ((f==false) && (i<arr.Length))
+    {
+        if (x==arr[i]) f=true;
+        i++;
+    }
+    return f;
+}
+
+int[] array3 = GetArray(5, -10, 10);
+Console.WriteLine(String.Join(" ", array3));
+
+int n=GetNumberFromUser($"Введите число ", "Ошибка ввода!");
+
+if (Check(n, array3)) Console.WriteLine("да");
+else Console.WriteLine("нет");
+
