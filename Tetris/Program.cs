59b2666c89982edb44960a728c411b8daf03df9e
Oni <oni-mail@mail.ru>
2023-01-21 20:31:36 +0300

    tetris_figures

--- Tetris/Program.cs
+++ Tetris/Program.cs
@@ -5,6 +5,31 @@
     {0,0,0,0 },
     {1,1,1,1 },
 };
+int[,] pic_half_cross=new int[,]
+{
+    {0,0,0 },
+    {0,1,0 },
+    {1,1,1 },    
+};
+int[,] pic_cube=new int[,]
+{
+    {1,1 },
+    {1,1 },
+};
+
+int[,] pic_z_right=new int[,]
+{
+    {1,1,0 },
+    {0,1,1 },
+    {0,0,0 }
+};
+
+int[,] pic_z_left=new int[,]
+{
+    {0,1,1 },
+    {1,1,0 },
+    {0,0,0 }
+};
 
 void PrintArray(int[,] array)
 {
@@ -38,16 +63,15 @@ void RotateTet_main(int[,] array)
 
 void RotateTet_second(int[,] array)
 {   
-    int i=0;
-    for (i=array.GetLength(0)-i; i<array.GetLength(0); i++)
+    for (int i=0; i<array.GetLength(0)-1; i++)
     {
-        for (int j=0; j<array.GetLength(1); j++)
+        for (int j=0; j<array.GetLength(1)-i; j++)
         {
             // int ch=0;
             // ch=array[i,j];
-            // array[i,j]=array[j,i];
-            // array[j,i]=ch;
-            (array[i,j], array[array.GetLength(0)+1-i,array.GetLength(1)+1-j])=(array[array.GetLength(0)+1-i,array.GetLength(1)+1-j], array[i,j]);
+            // array[i,j]=array[array.GetLength(0)-1-i,array.GetLength(1)-1-j];
+            // array[array.GetLength(0)-1-i,array.GetLength(1)-1-j]=ch;
+            (array[i,j], array[array.GetLength(0)-1-i,array.GetLength(1)-1-j])=(array[array.GetLength(0)-1-i,array.GetLength(1)-1-j], array[i,j]);
 
         }
     }
@@ -64,3 +88,23 @@ RotateTet_main(pic_line);
 PrintArray(pic_line);
 RotateTet_second(pic_line);
 PrintArray(pic_line);
+RotateTet_main(pic_line);
+PrintArray(pic_line);
+
+PrintArray(pic_half_cross);
+RotateTet_main(pic_half_cross);
+PrintArray(pic_half_cross);
+RotateTet_second(pic_half_cross);
+PrintArray(pic_half_cross);
+RotateTet_main(pic_half_cross);
+PrintArray(pic_half_cross);
+
+PrintArray(pic_cube);
+
+PrintArray(pic_z_right);
+RotateTet_main(pic_z_right);
+PrintArray(pic_z_right);
+RotateTet_second(pic_z_right);
+PrintArray(pic_z_right);
+RotateTet_main(pic_z_right);
+PrintArray(pic_z_right);
