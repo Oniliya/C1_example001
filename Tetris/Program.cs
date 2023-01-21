0ddbcbc8c4b7af81d234129cfc718c6ee64523f3
Oni <oni-mail@mail.ru>
2023-01-21 19:27:45 +0300

    tetris_1

--- Tetris/Program.cs
+++ Tetris/Program.cs
@@ -1,44 +1,66 @@
-﻿char[,] pic =new char[,]
+﻿int[,] pic_line =new int[,]
 {
-    {'.', '.', '.', '.' },
-    {'.', '.', '.', '.' },
-    {'.', '.', '.', '.' },
-    {'#', '#', '#', '#' },
+    {0,0,0,0 },
+    {0,0,0,0 },
+    {0,0,0,0 },
+    {1,1,1,1 },
 };
 
-void PrintArray(char[,] array)
+void PrintArray(int[,] array)
 {
     for (int i=0; i<array.GetLength(0); i++)
     {
         for (int j=0; j<array.GetLength(1); j++)
         {
-            Console.Write($"{array[i,j]} "); 
+            if (array[i,j]==1) Console.Write($"*");
+            else Console.Write($" ");
         }
         Console.WriteLine();
     }
     Console.WriteLine();
 }
 
-void RotateTet(char[,] array)
-{
-    char ch='/';
+void RotateTet_main(int[,] array)
+{    
     for (int i=0; i<array.GetLength(0); i++)
     {
-        for (int j=0; j<array.GetLength(1); j++)
+        for (int j=i+1; j<array.GetLength(1); j++)
         {
-            ch=array[i,j];
-            array[i,j]=array[j,i];
-            array[j,i]=ch;
+            // int ch=0;
+            // ch=array[i,j];
+            // array[i,j]=array[j,i];
+            // array[j,i]=ch;
+            (array[i,j], array[j,i])=(array[j,i], array[i,j]);
+
         }
     }
 }
+
+void RotateTet_second(int[,] array)
+{   
+    int i=0;
+    for (i=array.GetLength(0)-i; i<array.GetLength(0); i++)
+    {
+        for (int j=0; j<array.GetLength(1); j++)
+        {
+            // int ch=0;
+            // ch=array[i,j];
+            // array[i,j]=array[j,i];
+            // array[j,i]=ch;
+            (array[i,j], array[array.GetLength(0)+1-i,array.GetLength(1)+1-j])=(array[array.GetLength(0)+1-i,array.GetLength(1)+1-j], array[i,j]);
+
+        }
+    }
+}
+
 // void PressKey()
 // {
 
 // }
 
 Console.Clear();
-PrintArray(pic);
-RotateTet(pic);
-//PrintArray(pic);
-
+PrintArray(pic_line);
+RotateTet_main(pic_line);
+PrintArray(pic_line);
+RotateTet_second(pic_line);
+PrintArray(pic_line);
