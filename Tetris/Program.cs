eee44c8ba28a64d5fa2ddfcd1852810eafe39835
Oni <oni-mail@mail.ru>
2023-01-21 18:14:34 +0300

    tetris_1

--- Tetris/Program.cs
+++ Tetris/Program.cs
@@ -1,2 +1,44 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿char[,] pic =new char[,]
+{
+    {'.', '.', '.', '.' },
+    {'.', '.', '.', '.' },
+    {'.', '.', '.', '.' },
+    {'#', '#', '#', '#' },
+};
+
+void PrintArray(char[,] array)
+{
+    for (int i=0; i<array.GetLength(0); i++)
+    {
+        for (int j=0; j<array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i,j]} "); 
+        }
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+}
+
+void RotateTet(char[,] array)
+{
+    char ch='/';
+    for (int i=0; i<array.GetLength(0); i++)
+    {
+        for (int j=0; j<array.GetLength(1); j++)
+        {
+            ch=array[i,j];
+            array[i,j]=array[j,i];
+            array[j,i]=ch;
+        }
+    }
+}
+// void PressKey()
+// {
+
+// }
+
+Console.Clear();
+PrintArray(pic);
+RotateTet(pic);
+//PrintArray(pic);
+
