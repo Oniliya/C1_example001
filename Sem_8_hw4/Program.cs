2fa42f1b3a362a7a3c564ac14a4487a1a47cc550
Oni <oni-mail@mail.ru>
2023-01-27 19:38:08 +0300

    sem_8_hw4_done

---
+++ Sem_8_hw4/Program.cs
@@ -0,0 +1,63 @@
+﻿// Задача 60. ...Сформируйте трёхмерный массив из неповторяющихся двузначных чисел. Напишите программу, которая будет построчно выводить массив, добавляя индексы каждого элемента.
+// Массив размером 2 x 2 x 2
+// 66(0,0,0) 25(0,1,0)
+// 34(1,0,0) 41(1,1,0)
+// 27(0,0,1) 90(0,1,1)
+// 26(1,0,1) 55(1,1,1)
+
+
+int GetNumberFromUser(string message, string errorMessage)
+{
+    while (true)
+    {
+        Console.Write(message);
+        bool isCorrect = int.TryParse(Console.ReadLine(), out int userNumber);
+        if (isCorrect)
+            return userNumber;
+        Console.WriteLine(errorMessage);
+    }
+}
+
+int[,,] GetArray(int m, int n, int l, int minValue, int maxValue)
+{
+    int[,,] result = new int[m, n, l];
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            for (int k = 0; k < l; k++)
+            {
+                result[i, j, k] = new Random().Next(minValue, maxValue + 1);
+            }
+        }
+    }
+    return result;
+}
+
+void PrintArray(int[,,] inArray)
+{
+    for (int k = 0; k < inArray.GetLength(2); k++)
+    {
+        for (int i = 0; i < inArray.GetLength(0); i++)
+        {
+            for (int j = 0; j < inArray.GetLength(1); j++)
+            {
+                Console.Write($"{inArray[i, j, k]}({i},{j},{k}) ");
+            }
+            Console.WriteLine();
+        }
+        Console.WriteLine();
+    }
+}
+
+
+Console.Clear();
+int rows = GetNumberFromUser("Введите количество строк в массиве -> ","Ошибка ввода");
+int columns = GetNumberFromUser("Введите количество столбцов в массиве -> ","Ошибка ввода");
+int levels = GetNumberFromUser("Введите количество уровней в массиве -> ","Ошибка ввода");
+
+int[,,] array = GetArray(rows, columns, levels, 0, 50);
+PrintArray(array);
+Console.WriteLine();
+
+
