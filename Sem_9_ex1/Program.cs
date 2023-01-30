bcb1c47322e725976672121346d83a2b3c87ea1d
Oni <oni-mail@mail.ru>
2023-01-30 20:01:20 +0300

    sem_9_ex1_3

--- Sem_9_ex1/Program.cs
+++ Sem_9_ex1/Program.cs
@@ -33,9 +33,24 @@ void F_posl2(int n1, int n2)
     }
 }
 
+int RecurrSum(int n3)
+{
+    if (n3==0) return 0;
+    else 
+    {
+        return n3%10+RecurrSum(n3/10);
+    }
+
+}
+
 int num = GetNumberFromUser("Введите число -> ","Ошибка ввода");
 F_posl(num);
 Console.WriteLine();
 int num2 = GetNumberFromUser("Введите второе значение -> ","Ошибка ввода");
 if (num2>num) F_posl2(num, num2);
 else F_posl2(num2, num);
+Console.WriteLine();
+
+int num3 = GetNumberFromUser("Введите число -> ","Ошибка ввода");
+Console.WriteLine($"{RecurrSum(num3)}");
+
