56ae91f4879ae2f17f74ee5f7fdc4e928298af29
Oni <oni-mail@mail.ru>
2023-01-30 19:44:38 +0300

    sem_9_ex1

---
+++ Sem_9_ex1/Program.cs
@@ -0,0 +1,41 @@
+﻿int GetNumberFromUser(string message, string errorMessage)
+{
+    while (true)
+    {
+        Console.Write(message);
+        bool isCorrect = int.TryParse(Console.ReadLine(), out int userNumber);
+        if (isCorrect)
+            return userNumber;
+        Console.WriteLine(errorMessage);
+    }
+}
+
+void F_posl(int n1)
+{
+    int n=n1;
+    if (n==0) return;
+    else 
+    {
+        F_posl(n-1);
+        Console.Write($"{n}, ");
+    }
+}
+
+void F_posl2(int n1, int n2)
+{
+    int n=n2;
+    
+    if (n==n1-1) return;
+    else 
+    {
+        F_posl2(n1,n-1);
+        Console.Write($"{n}, ");    
+    }
+}
+
+int num = GetNumberFromUser("Введите число -> ","Ошибка ввода");
+F_posl(num);
+Console.WriteLine();
+int num2 = GetNumberFromUser("Введите второе значение -> ","Ошибка ввода");
+if (num2>num) F_posl2(num, num2);
+else F_posl2(num2, num);
