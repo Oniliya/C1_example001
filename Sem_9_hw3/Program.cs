37d5f5dd48e49d371f1fe8bc062e2d674454ae37
Oni <oni-mail@mail.ru>
2023-02-02 20:22:23 +0300

    sem_9_hw_all2

--- Sem_9_hw3/Program.cs
+++ Sem_9_hw3/Program.cs
@@ -9,29 +9,13 @@
         Console.WriteLine(errorMessage);
     }
 }
-int power (int y, int x)
-{
-    int mult=y;
-    if (x==0) return 1;
-    else 
-    {
-        for (int i=1; i<=x; i++)
-        {
-            mult=mult*y;
-        }
-        return mult;
-    }
-}
 
 int RecurrAkkerman(int m, int n)
 {
     if (m == 0) return n + 1;
-    if (m == 1) return 2 + n;
-    if (m == 2) return 2*n + 3;
-    if (m == 3) return power(2, n+3) - 3;
-    //if ((m>0)&&(n>0)) return RecurrAkkerman(m-1,RecurrAkkerman(m,n-1));
+    else if ((m > 0) && (n == 0)) return RecurrAkkerman(m - 1, 1);
+    else return RecurrAkkerman(m - 1, RecurrAkkerman(m, n - 1));
 }
-
 
 
 // Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
